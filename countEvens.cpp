5565d5d8d85c680561e4922126faa25f1ee1afac
JohnnJudge <56047717+JohnnJudge@users.noreply.github.com>
2019-11-05 16:13:45 -0800

    Update countEvens.cpp

--- countEvens.cpp
+++ countEvens.cpp
@@ -1,14 +1,18 @@
 #include "arrayFuncs.h"
+
 #include <cstdlib>
+
 #include <iostream>
+
 #include"utility.h"
+
 bool isEven(int);
 int countEvens(int a[], int size) {
- 	int count = 0;
-	for(int i = 0; i < size; i++){
-		if(isEven(a[i])){
-			count++;
-		}
- 	 }
-	 return count;
+  int count = 0;
+  for (int i = 0; i < size; i++) {
+    if (isEven(a[i])) {
+      count++;
+    }
+  }
+  return count;
 }
