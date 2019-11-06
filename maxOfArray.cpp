ddd3a43c774aaa3982a7a1496d54291a301687ee
JohnnJudge <56047717+JohnnJudge@users.noreply.github.com>
2019-11-05 16:14:57 -0800

    Update maxOfArray.cpp

--- maxOfArray.cpp
+++ maxOfArray.cpp
@@ -3,16 +3,16 @@
 #include <iostream>
 
 int maxOfArray(int a[], int size) {
-	if (size < 1) {
-		std::cerr << "ERROR: maxOfArray called with size < 1" << std::endl;
-		exit(1);
-	 }
+  if (size < 1) {
+    std::cerr << "ERROR: maxOfArray called with size < 1" << std::endl;
+    exit(1);
+  }
 
-	int max = a[0];
-	for(int i = 1; i < size; i++){
-		if(a[i] > max){
-			max = a[i];
-		}
-	}
-	return max;
+  int max = a[0];
+  for (int i = 1; i < size; i++) {
+    if (a[i] > max) {
+      max = a[i];
+    }
+  }
+  return max;
 }
