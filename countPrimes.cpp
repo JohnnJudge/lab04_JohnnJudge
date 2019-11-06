b7c48706cb432b0c9a28246a7ac7f71a94de9ad4
JohnnJudge <56047717+JohnnJudge@users.noreply.github.com>
2019-11-05 16:14:25 -0800

    Update countPrimes.cpp

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -1,15 +1,14 @@
 #include "arrayFuncs.h"
 #include <cstdlib>
 #include <iostream>
-
 #include "utility.h"
 bool isPrime(int);
 int countPrimes(int a[], int size) {
-	int count = 0;
-	for(int i = 0; i < size; i++){
-		if(isPrime(a[i])){
-			count++;
-		}
-	}
-	return count;
+  int count = 0;
+  for (int i = 0; i < size; i++) {
+    if (isPrime(a[i])) {
+      count++;
+    }
+  }
+  return count;
 }
